3b82b8228e59942ddc9819686b13984bb07a7f8b
babaHedz <hedi.rivas.pro@gmail.com>
2024-08-05 22:55:38 +0200

    pagination done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { Toaster } from "react-hot-toast";
 import { useDebounce, useJobItems } from "../lib/hooks";
+import { RESULT_PER_PAGE } from "../lib/constants";
 import Background from "./Background";
 import BookmarksButton from "./BookmarksButton";
 import Container from "./Container";
@@ -13,15 +15,30 @@ import ResultsCount from "./ResultsCount";
 import SearchForm from "./SearchForm";
 import Sidebar, { SidebarTop } from "./Sidebar";
 import SortingControls from "./SortingControls";
-import { Toaster } from "react-hot-toast";
 
 function App() {
+  // state
   const [searchText, setSearchText] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
   const debouncedSearchText = useDebounce(searchText);
   const { jobItems, isLoading } = useJobItems(debouncedSearchText);
 
+  // derived state
   const totalNumberOfResults = jobItems.length;
-  const jobItemsSliced = jobItems.slice(0, 7);
+  const totalNumberOfPages = Math.ceil(totalNumberOfResults / RESULT_PER_PAGE);
+  const jobItemsSliced = jobItems?.slice(
+    currentPage * RESULT_PER_PAGE - RESULT_PER_PAGE,
+    currentPage * RESULT_PER_PAGE
+  );
+
+  // handlers
+  const handleChangePage = (direction: "next" | "previous") => {
+    if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === "previous") {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
 
   return (
     <>
@@ -45,7 +62,11 @@ function App() {
 
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
 
-          <PaginationControls />
+          <PaginationControls
+            currentPage={currentPage}
+            onClick={handleChangePage}
+            totalNumberOfPages={totalNumberOfPages}
+          />
         </Sidebar>
         <JobItemContent />
       </Container>
